ed6bfc63cbe56c114975032b9f31b2478a6d49c2
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-11-23 10:41:12 +0100

    Implemented PIRSensor Class

---
+++ src/components/api/PIRSensor.hpp
@@ -0,0 +1,14 @@
+#ifndef __PIR_SENSOR__
+#define __PIR_SENSOR__
+
+class PIRSensor
+{
+private:
+    const int pin;
+public:
+    PIRSensor(int pin);
+    ~PIRSensor();
+    int read();
+};
+
+#endif
---
+++ src/components/impl/PIRSensor.cpp
@@ -0,0 +1,17 @@
+#include "../api/PIRSensor.hpp"
+
+#include <Arduino.h>
+
+PIRSensor::PIRSensor(int pin): pin(pin)
+{
+    pinMode(pin, INPUT);
+}
+
+PIRSensor::~PIRSensor()
+{
+}
+
+int PIRSensor::read()
+{
+    return digitalRead(pin);
+}
