0cd0064fcfcea5a0fa932d528a2e098823ec21cf
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-11-23 09:47:46 +0100

    created led class

---
+++ src/components/api/Led.hpp
@@ -0,0 +1,13 @@
+#ifndef __LED__
+#define __LED__
+
+class Led
+{
+private:
+    /* data */
+public:
+    Led(/* args */);
+    ~Led();
+};
+
+#endif
---
+++ src/components/impl/Led.cpp
@@ -0,0 +1,5 @@
+#include "../api/led.hpp"
+
+Led::Led(){
+
+}
