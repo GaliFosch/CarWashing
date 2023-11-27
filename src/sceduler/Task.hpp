85c7f99050decf34bf1ad9b5e12905c93b95c0a3
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-11-27 15:20:37 +0100

    COMPILAAAAAA!!!!

--- src/sceduler/Task.hpp
+++ src/sceduler/Task.hpp
@@ -6,23 +6,24 @@ class Task
 protected:
     unsigned int period;
     unsigned int timeCounter;
+
 public:
-    virtual void init(unsigned int period);
-    bool updateAndCheckTime(unsigned int time);
+    virtual void init(unsigned int period)
+    {
+        period = period;
+    }
+    bool updateAndCheckTime(unsigned int time)
+    {
+        timeCounter += time;
+        if (timeCounter >= period)
+        {
+            timeCounter = 0;
+            return true;
+        }
+        return false;
+    }
     virtual void tick() = 0;
 };
 
-void Task::init(unsigned int period){
-    period = period;
-}
-
-bool Task::updateAndCheckTime(unsigned int time){
-    timeCounter+=time;
-    if(timeCounter>=period){
-        timeCounter = 0;
-        return true;
-    }
-    return false;
-}
 
 #endif
