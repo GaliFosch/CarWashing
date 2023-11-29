4f66417a5785c9f39774fb7d1ec6f8679023363f
Galileo Foschini <galileo.foschini@studio.unibo.it>
2023-11-29 13:03:24 +0100

    Style changes

--- src/components/impl/ServoMotor.cpp
+++ src/components/impl/ServoMotor.cpp
@@ -1,20 +1,24 @@
 #include "../api/ServoMotorImpl.hpp"
 
-ServoMotorImpl::ServoMotorImpl(int pin){
-  this->pin = pin;  
-} 
-
-void ServoMotorImpl::on(){
-  motor.attach(pin);    
+ServoMotorImpl::ServoMotorImpl(int pin)
+{
+  this->pin = pin;
 }
 
-void ServoMotorImpl::setPosition(int angle){
-  // 750 -> 0, 2250 -> 180 
+void ServoMotorImpl::on()
+{
+  motor.attach(pin);
+}
+
+void ServoMotorImpl::setPosition(int angle)
+{
+  // 750 -> 0, 2250 -> 180
   // 750 + angle*(2250-750)/180
-  static float coeff = (2250.0-750.0)/180;
+  static float coeff = (2250.0 - 750.0) / 180;
   motor.write(angle);
 }
 
-void ServoMotorImpl::off(){
-  motor.detach();    
+void ServoMotorImpl::off()
+{
+  motor.detach();
 }
